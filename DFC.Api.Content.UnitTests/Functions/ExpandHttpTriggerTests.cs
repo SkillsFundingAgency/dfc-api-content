1019ffc55992070e0ab4b96099d8da9bf5444d80
ryanbarlowmethods <ryan.barlow@methods.co.uk>
2022-03-25 14:39:49 +0000

    Added another test

--- DFC.Api.Content.UnitTests/Functions/ExpandHttpTriggerTests.cs
+++ DFC.Api.Content.UnitTests/Functions/ExpandHttpTriggerTests.cs
@@ -123,6 +123,69 @@ namespace DFC.Api.Content.UnitTests.Functions
                 JsonConvert.SerializeObject(JToken.Parse(expectedJsonOutput)), 
                 actualJsonOutput);
         }
+        
+        [Fact]
+        public async Task Expand_GetPageTaxonomyOnly_ReturnsCorrectJsonResponse()
+        {
+            var expectedJsonOutput = File.ReadAllText(Directory.GetCurrentDirectory() + "/Files/Output/PageRecordOutput_ExpandTaxonomyOnly.json");
+            
+            var recordJsonInputPage = File.ReadAllText(Directory.GetCurrentDirectory() + "/Files/Input/PageRecordInput_Expand_Page.json");
+            A.CallTo(() => _dataSource.Run(A<GenericQuery>.That.Matches(x => x.ContentType == "page")))
+                .Returns(new List<Dictionary<string, object>>
+                {
+                    JsonConvert.DeserializeObject<Dictionary<string, object>>(recordJsonInputPage)
+                });
+            
+            var recordJsonInputPageLocation1 = File.ReadAllText(Directory.GetCurrentDirectory() + "/Files/Input/PageRecordInput_Expand_PageLocation1.json");
+            A.CallTo(() => _dataSource.Run(A<GenericQuery>.That.Matches(x => x.QueryText.Contains("c78a4c9c-df64-4ead-a307-ba73165b7286"))))
+                .Returns(new List<Dictionary<string, object>>
+                {
+                    JsonConvert.DeserializeObject<Dictionary<string, object>>(recordJsonInputPageLocation1)
+                });
+            
+            var recordJsonInputPageLocation2 = File.ReadAllText(Directory.GetCurrentDirectory() + "/Files/Input/PageRecordInput_Expand_PageLocation2.json");
+            A.CallTo(() => _dataSource.Run(A<GenericQuery>.That.Matches(x => x.QueryText.Contains("48b3f8cb-27c5-4e3a-9a53-69b6cfe8e408"))))
+                .Returns(new List<Dictionary<string, object>>
+                {
+                    JsonConvert.DeserializeObject<Dictionary<string, object>>(recordJsonInputPageLocation2)
+                });          
+
+            var recordJsonInputPageLocation3 = File.ReadAllText(Directory.GetCurrentDirectory() + "/Files/Input/PageRecordInput_Expand_PageLocation3.json");
+            A.CallTo(() => _dataSource.Run(A<GenericQuery>.That.Matches(x => x.QueryText.Contains("a6d20c3c-51c0-437a-84c4-ae23cc78c99a"))))
+                .Returns(new List<Dictionary<string, object>>
+                {
+                    JsonConvert.DeserializeObject<Dictionary<string, object>>(recordJsonInputPageLocation3)
+                });            
+            
+            var recordJsonInputTaxonomy = File.ReadAllText(Directory.GetCurrentDirectory() + "/Files/Input/PageRecordInput_Expand_Taxonomy.json");
+            A.CallTo(() => _dataSource.Run(A<GenericQuery>.That.Matches(x => x.ContentType == "taxonomy")))
+                .Returns(new List<Dictionary<string, object>>
+                {
+                    JsonConvert.DeserializeObject<Dictionary<string, object>>(recordJsonInputTaxonomy)
+                });
+            
+            _request.Body = new MemoryStream();
+            
+            var messageBytes = Encoding.UTF8.GetBytes(@"{
+                ""MultiDirectional"": true,
+                ""MaxDepth"": 5,
+                ""TypesToInclude"": [""taxonomy""]
+            }");
+            
+            _request.Body.Write(messageBytes, 0, messageBytes.Length);
+            
+            var result = await RunFunction("page", Guid.Parse("f60c7388-7b89-40d6-9911-52f5ae6b4a41"));
+            
+            // Assert
+            Assert.IsType<OkObjectResult>(result);
+
+            var okObjectResult = result as OkObjectResult;
+            var actualJsonOutput = JsonConvert.SerializeObject(okObjectResult!.Value);
+
+            Assert.Equal(
+                JsonConvert.SerializeObject(JToken.Parse(expectedJsonOutput)), 
+                actualJsonOutput);
+        }
 
         private async Task<IActionResult> RunFunction(string contentType, Guid id)
         {
